24d8d89d68d31de82d674d3ef5dbab30171b82bc
Rick Soares <146774911+Ricksrss@users.noreply.github.com>
2023-11-18 17:54:23 -0300

    Criação da pagina de cadastro

---
+++ src/app/cadastro/page.jsx
@@ -0,0 +1,12 @@
+import CadastroForm from '../../components/CadastroForm/index';
+
+const CadastroPage = () => {
+    return (
+        <div>
+            <h1>Cadastro</h1>
+            <CadastroForm/>
+        </div>
+    )
+};
+
+export default CadastroPage;
