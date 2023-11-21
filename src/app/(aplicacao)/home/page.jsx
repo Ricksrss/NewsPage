d12f8c59c49efd6f8453dc621f33abddddc1effd
Rick Soares <146774911+Ricksrss@users.noreply.github.com>
2023-11-20 22:03:58 -0300

    configurações da home

--- src/app/(aplicacao)/home/page.jsx
+++ src/app/(aplicacao)/home/page.jsx
@@ -1,10 +1,32 @@
+import Noticia from "../../../components/Noticia";
+
+const noticias = [
+  {
+    id: 1,
+    img: "http://via.placeholder.com/150",
+    texto: "Texto da <b>noticia 1</b>",
+  },
+  {
+    id: 2,
+    img: "http://via.placeholder.com/150",
+    texto: "Texto da <b>noticia 2</b>",
+  },
+  {
+    id: 3,
+    img: "http://via.placeholder.com/150",
+    texto: "Texto da <b>noticia 3</b>",
+  },
+];
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      {noticias.map((noticia) => (
+        <Noticia key={noticia.id} noticia={noticia} />
+      ))}
+    </div>
+  );
+};
 
 export default HomePage;
